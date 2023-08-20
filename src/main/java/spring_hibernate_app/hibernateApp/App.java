1db392d07389ce1337e369f6ca3e45bf374b785e
Alexander <sasha0435@gmail.com>
2023-08-20 15:56:15 +0300

    lesson-48(первое приложение с Hibernate)

---
+++ src/main/java/spring_hibernate_app/hibernateApp/App.java
@@ -0,0 +1,33 @@
+package spring_hibernate_app.hibernateApp;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import spring_hibernate_app.hibernateApp.model.Person;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        
+        session.beginTransaction();
+        
+        Person person = session.get(Person.class, 1);
+        
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+        
+        session.getTransaction().commit();
+        
+        sessionFactory.close();
+    }
+}
